8f7c0959ef85632ee84b3bc97cc774b9ffbfd561
Alexis CHAPPRON <bullrot-10@hotmail.fr>
2026-03-30 17:39:13 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,87 @@
+import * as React from 'react';
+import { ScrollView, StyleSheet, Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { enableScreens } from 'react-native-screens';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {useMemo} from "react";
 
-export default function App() {
+enableScreens();
+
+function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView>
+        <Text>Home Screen</Text>
+        {Array(400).fill(true).map((_item, index) => (<Text key={index}>Home item {index}</Text>))}
+        <StatusBar style="auto" />
+      </ScrollView>
     </View>
   );
 }
+
+function RandomScreen() {
+  return (
+    <ScrollView>
+      <Text>Random Screen</Text>
+      {Array(400).fill(true).map((_item, index) => (<Text key={index}>Random item {index}</Text>))}
+      <StatusBar style="auto" />
+    </ScrollView>
+  );
+}
+
+
+function SettingsScreen() {
+  return (
+    <ScrollView>
+      <Text>Settings Screen</Text>
+      {Array(400).fill(true).map((_item, index) => (<Text key={index}>Setting Item {index}</Text>))}
+      <StatusBar style="auto" />
+    </ScrollView>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+
+    <NavigationContainer>
+      <HomeStackNavigator.Navigator
+        initialRouteName="Tabs"
+      >
+        <HomeStackNavigator.Screen
+          name="Tabs"
+          component={BottomTabs}
+          options={({ route }) => ({
+            headerShown: false,
+          })}
+        />
+      </HomeStackNavigator.Navigator>
+
+    </NavigationContainer>
+  );
+}
+
+export const HomeStackNavigator =
+  createNativeStackNavigator();
+
+
+const BottomTabs = () => {
+
+  return useMemo(
+    () => (
+      <Tab.Navigator screenOptions={{ headerShown: true, animation: "shift"}} detachInactiveScreens={true}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Random" component={RandomScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    ),
+    [],
+  );
+};
+
 
 const styles = StyleSheet.create({
   container: {
